3ddbf0256e74ab26407d0342ea6d3d93ee9a8b40
psy0316 <seigram0884@gmail.com>
2022-12-29 18:45:06 +0900

    init

---
+++ java/consumer-demo/src/main/java/org/example/RebalanceListener.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
+import org.apache.kafka.common.TopicPartition;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+
+public class RebalanceListener implements ConsumerRebalanceListener {
+
+    private final static Logger logger = LoggerFactory.getLogger(RebalanceListener.class);
+
+    //before Rebalance partition info
+    @Override
+    public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+        logger.warn("Partitions are revoked : " + partitions.toString());
+    }
+
+
+    //after Rebalance partition info
+    @Override
+    public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+        logger.warn("Partitions are assigned : " + partitions.toString());
+    }
+}
